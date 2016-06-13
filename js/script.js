e0e64f20443c402e0076c8532e2843d7adceb55e
ydeepk <ydeepkcs@gmail.com>
2016-06-13 15:17:37 +0530

    till modal window

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-
-    
-    $(window).scroll(function() {
+$(window).scroll(function() {
     var scroll = $(window).scrollTop();
         
         if(scroll>=20) {
@@ -13,7 +11,26 @@
 });
 
 
+$(document).ready(function() {
     
-
-
-$(document).ready();
+    $('.scrollspy').scrollSpy();    
+    
+    $('.modal-trigger').leanModal();
+    
+    /* smooth scroll */
+    $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+    
+});
