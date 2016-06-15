b76bbf5e4aa44dfdbd127ee9bd3991272ee63362
ydeepk <ydeepkcs@gmail.com>
2016-06-15 10:11:25 +0530

    get info page

--- js/script.js
+++ js/script.js
@@ -1,41 +1,40 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-        
-        if(scroll>=20) {
-            $('.move-navbar').addClass('hide');
-            $('#navbar-fixed').removeClass('hide');
-        } else {
-            $('#navbar-fixed').addClass('hide');
-            $('.move-navbar').removeClass('hide');
-        }
+
+    if (scroll >= 20) {
+        $('.move-navbar').addClass('hide');
+        $('#navbar-fixed').removeClass('hide');
+    } else {
+        $('#navbar-fixed').addClass('hide');
+        $('.move-navbar').removeClass('hide');
+    }
 });
 
 
 $(document).ready(function() {
-    
+
     $('.close-this').click(function() {
-        $('#lets-work-together-modal').closeModal();
         $('#message').closeModal();
     });
-    
-    $('.scrollspy').scrollSpy();    
-    
+
+    $('.scrollspy').scrollSpy();
+
     $('.modal-trigger').leanModal();
-    
+
     /* smooth scroll */
     $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-    
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
 });
