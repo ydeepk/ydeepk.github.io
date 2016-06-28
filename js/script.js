4dd3b5be45a3f4e7dc563cb433397054b2032101
ydeepk <ydeepkcs@gmail.com>
2016-06-28 17:08:27 +0530

    already remove minified files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
-$(window).scroll(function(){var e=$(window).scrollTop();e>=20?($(".move-navbar").addClass("hide"),$("#navbar-fixed").removeClass("hide")):($("#navbar-fixed").addClass("hide"),$(".move-navbar").removeClass("hide"))}),$(document).ready(function(){$(window).load(function(){$(".preloader").remove()}),$(".close-this").click(function(){$("#message").closeModal()}),$(".scrollspy").scrollSpy(),$(".modal-trigger").leanModal(),$(function(){$('a[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var e=$(this.hash);if(e=e.length?e:$("[name="+this.hash.slice(1)+"]"),e.length)return $("html, body").animate({scrollTop:e.offset().top},1e3),!1}})})});
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 20) {
+        $('.move-navbar').addClass('hide');
+        $('#navbar-fixed').removeClass('hide');
+    } else {
+        $('#navbar-fixed').addClass('hide');
+        $('.move-navbar').removeClass('hide');
+    }
+});
+
+
+$(document).ready(function() {
+
+    $(window).load(function() {
+      $('.preloader').remove();
+    });
+
+    $('.close-this').click(function() {
+        $('#message').closeModal();
+    });
+
+    $('.scrollspy').scrollSpy();
+
+    $('.modal-trigger').leanModal();
+
+    /* smooth scroll */
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+});
