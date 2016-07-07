3ad4338cc3009fca79c42e668f8114d09e22ef19
ydeepk <ydeepkcs@gmail.com>
2016-07-07 08:48:46 +0530

    google analytics

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
-$(window).scroll(function(){var a=$(window).scrollTop();a>=20?($(".move-navbar").addClass("hide"),$("#navbar-fixed").removeClass("hide")):($("#navbar-fixed").addClass("hide"),$(".move-navbar").removeClass("hide"))}),$(document).ready(function(){$(window).load(function(){$(".preloader").remove()}),$(".close-this").click(function(){$("#message").closeModal()}),$(".scrollspy").scrollSpy(),$(".modal-trigger").leanModal(),$(function(){$('a[href*="#"]:not([href="#"])').click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var a=$(this.hash);if(a=a.length?a:$("[name="+this.hash.slice(1)+"]"),a.length)return $("html, body").animate({scrollTop:a.offset().top},1e3),!1}})})});
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 20) {
+        $('.move-navbar').addClass('hide');
+        $('#navbar-fixed').removeClass('hide');
+    } else {
+        $('#navbar-fixed').addClass('hide');
+        $('.move-navbar').removeClass('hide');
+    }
+});
+
+
+$(document).ready(function() {
+
+    $(window).load(function() {
+      $('.preloader').remove();
+    });
+
+    $('.close-this').click(function() {
+        $('#message').closeModal();
+    });
+
+    $('.scrollspy').scrollSpy();
+
+    $('.modal-trigger').leanModal();
+
+    /* smooth scroll */
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+});
