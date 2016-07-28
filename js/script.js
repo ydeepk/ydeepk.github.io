bb6393d2ae6bec26fa7fd9871ee83981c03a3152
ydeepk <ydeepkcs@gmail.com>
2016-07-28 22:21:51 +0530

    add all new

---
+++ js/script.js
@@ -0,0 +1,52 @@
+/* Fixed Navbar */
+$(window).bind('scroll', function() {
+    var navHeight = $(window).height() - 70;
+    if ($(window).scrollTop() > navHeight) {
+        $('.fix').addClass('fixed');
+    } else {
+        $('.fix').removeClass('fixed');
+    }
+});
+
+
+$(document).ready(function() {
+
+    $(window).load(function() {
+      $('.preloader').remove();
+    });
+
+    $('.close').click(function() {
+        $('#hire-me-window').closeModal();
+    });
+
+    $('.scrollspy').scrollSpy();
+
+    $('.modal-trigger').leanModal();
+
+    /* $().animateCss();
+    $().addClass('animated fadein');
+    */
+
+  /*animation*/
+  $('.title').delay(6000).addClass('animated bounceIn');
+  $('#desig').delay(6400).addClass('animated fadeInRight');
+  $('#quote').delay(7000).addClass('animated fadeInLeft');
+  $('#hire').delay().addClass('animated bounceIn');
+
+    /* smooth scroll */
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+});
