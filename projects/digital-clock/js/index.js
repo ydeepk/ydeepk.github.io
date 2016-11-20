d01766fa9a1e31a4e1a037e5d68552235386c481
ydeepk <ydeepkcs@gmail.com>
2016-11-20 22:29:26 +0530

    added favicon

---
+++ projects/digital-clock/js/index.js
@@ -0,0 +1,11 @@
+/*
+  main js file
+  Title : Digital Clock
+  Author : ydeepk | Deepak Yadav
+  website : https://ydeepk.github.io
+  licinse: licnese.txt
+  resources : ( gulpjs, clockjs, momentjs)
+
+*/
+
+document.getElementById('clock').innerHTML = moment().format();
