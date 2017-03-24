ebac449244e5a900ef5b006bf387b7a999be23fa
ydeepk <ydeepkcs@gmail.com>
2017-03-24 11:29:09 +0530

    styles added and autoprefixer installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
 
 gulp.task('default', function() {
     // place code for your default task here
+    gulp.watch('sass/**/*.scss', ['styles'])
 });
+
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
         .pipe(gulp.dest('./css'))
 });
